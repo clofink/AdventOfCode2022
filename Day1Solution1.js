ea1bb963e752d6203587e11579ae5ab6aaa8b864
Connor <connor.lofink@gmail.com>
2022-12-02 00:54:18 -0500

    bugfix for day 1 to work on Mac

--- Day1Solution1.js
+++ Day1Solution1.js
@@ -1,6 +1,6 @@
 function day1Solution1(data) {
     let items = data.split('\r\n');
-    if (items.length < 1) items.split('\n'); // acts as a check for UNIX like endings (on my Mac)
+    if (items.length < 2) items = data.split('\n'); // acts as a check for UNIX like endings (on my Mac)
     items.push(""); // bit of a hack since there's no empty line after the last item so it never gets added to the total
     let totals = [];
     let tempTotal = 0;
