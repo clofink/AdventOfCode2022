aef3863b7bd1c2513d980883b3c2df75906da252
Connor <connor.lofink@gmail.com>
2022-12-07 01:18:56 -0500

    Day 7 solutions Made a dumb mistake and didn't realize that file/dir names could repeat which led to a bunch of changes that I needed to make and a rethink of how I was handling things

---
+++ Day7Solution1.js
@@ -0,0 +1,79 @@
+function day7Solution1(data) {
+    let lines = data.split("\r\n");
+    if (lines.length < 2) lines = data.split("\n");
+
+    let root = newDirectory(null, "/");
+
+    let currentDirectory;
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i];
+        let parts = line.split(' ');
+        if (parts[0] === "$") {
+            // this is a command, either cd or ls
+            if (parts[1] === 'cd') {
+                changeDirectory(parts[2]);
+            }
+            else if (parts[1] === 'ls') {
+                listFiles();
+            }
+        }
+        else {
+            if (parts[0] === "dir") {
+                currentDirectory.children.push(newDirectory(currentDirectory, parts[1]));
+            }
+            else {
+                // file with size
+                currentDirectory.children.push(newFile(currentDirectory, parts[1], parts[0]));
+            }
+        }
+    }
+
+    function changeDirectory(path) {
+        if (path === "/") {
+            currentDirectory = root;
+        }
+        else if (path === "..") {
+            currentDirectory = currentDirectory.parent;
+        }
+        else {
+            for (let file of currentDirectory.children) {
+                if (file.type === "dir" && file.name === path) {
+                    currentDirectory = file;
+                    break;
+                }
+            }
+        }
+    }
+
+    function listFiles() {
+        return;
+    }
+
+    function newDirectory(parent, name) {
+        return {name: name, type: "dir", parent: parent, children: [], size: 0};
+    }
+
+    function newFile(parent, name, size) {
+        let tempParent = parent
+        while (tempParent.parent != null || tempParent.name == "/") {
+            tempParent.size += parseInt(size);
+            tempParent = tempParent.parent ? tempParent.parent : "";
+        }
+        return {name: name, type: "file", parent: parent, size: parseInt(size)};
+    }
+
+    let total = 0;
+    let queue = [root];
+    while (queue.length > 0) {
+        let item = queue.shift();
+        if (item.size <= 100000) {
+            total += item.size;
+        }
+        for (let child of item.children) {
+            if (child.type == "dir") {
+                queue.push(child);
+            }
+        }
+    }
+    return total;
+}
